bc9ccefca1939efc464c50b0995f34751364c5b6
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-09 23:36:08 -0500

    Added extension method to convert Vector2Int to unity Vector2Int

---
+++ Client/Assets/Scripts/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using GiantScape.Common.Game;
+
+using Vector2IntUnity = UnityEngine.Vector2Int;
+
+namespace GiantScape.Client
+{
+    internal static class Extensions
+    {
+        public static Vector2IntUnity ToUnity(this Vector2Int self)
+        {
+            return new Vector2IntUnity(self.x, self.y);
+        }
+    }
+}
