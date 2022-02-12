f7e1644cf641e2620cdfd3a1e8d414dc43e6e937
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-11 22:22:42 -0500

    Added Server.Accounts namespace to handle user accounts

---
+++ Server/GiantScape.Server.Core/Accounts/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace GiantScape.Server.Accounts
+{
+    internal class Account
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+
+        public bool LoggedIn { get; set; }
+    }
+}
---
+++ Server/GiantScape.Server.Core/Accounts/LoginManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiantScape.Server.Accounts
+{
+    internal class LoginManager
+    {
+        public void RequestLogin(PlayerClient player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
