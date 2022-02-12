8c7d976bb75c8e47dd3eb2db0513ebefe5a0b587
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-11 22:49:38 -0500

    Added LoginPacket

---
+++ Server/GiantScape.Common/Net/Packets/LoginPacket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiantScape.Common.Net.Packets
+{
+    public class LoginPacket : NetworkPacket
+    {
+        public override PacketType Type => PacketType.Login;
+
+        public override ushort PacketLength => GetSize(Username, PasswordHash);
+
+        public string Username { get; private set; }
+        public string PasswordHash { get; private set; }
+
+        public override void FromBytes(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] GetContentBytes()
+        {
+            return ContentsToBytes(Username, PasswordHash);
+        }
+    }
+}
