54103f8065433306e1de4dafc254ed6fe292faa9
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-11 23:42:27 -0500

    Made PacketEvent serializable

--- Client/Assets/Scripts/Net/NetworkEvents.cs
+++ Client/Assets/Scripts/Net/NetworkEvents.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine.Events;
+﻿using System;
+
+using UnityEngine.Events;
 
 using GiantScape.Common.Net.Packets;
 
 namespace GiantScape.Client.Net
 {
+    [Serializable]
     public class PacketEvent : UnityEvent<NetworkPacket> { }
 }
