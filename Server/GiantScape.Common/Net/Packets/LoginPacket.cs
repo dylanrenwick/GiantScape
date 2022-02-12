601259312dfe93c2c9cb68c149876f96fc55e87d
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-11 23:30:04 -0500

    Added constructor to LoginPacket

--- Server/GiantScape.Common/Net/Packets/LoginPacket.cs
+++ Server/GiantScape.Common/Net/Packets/LoginPacket.cs
@@ -15,6 +15,12 @@ namespace GiantScape.Common.Net.Packets
         public string Username { get; private set; }
         public string PasswordHash { get; private set; }
 
+        public LoginPacket(string username, string passwordHash)
+        {
+            Username = username;
+            PasswordHash = passwordHash;
+        }
+
         public override void FromBytes(byte[] bytes)
         {
             string username = PacketEncoding.BytesToString(bytes);
