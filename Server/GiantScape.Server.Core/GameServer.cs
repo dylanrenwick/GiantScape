91494acb09fac425c8f44f0446d66e9b1c420310
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-11 22:50:14 -0500

    Added passthrough event to loginManager if player is not logged in

--- Server/GiantScape.Server.Core/GameServer.cs
+++ Server/GiantScape.Server.Core/GameServer.cs
@@ -69,6 +69,7 @@ namespace GiantScape.Server
             }
 
             PlayerClient player = players[client];
+            if (!player.IsLoggedIn) loginManager.HandlePacket(player, e.Packet);
         }
 
         private void AddPlayerClient(PlayerClient player)
