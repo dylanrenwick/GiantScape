2036875e9f850e6e84a8c0359b2c0aca775a3b0c
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-12 23:59:49 -0500

    Created GameEntity abstract class

---
+++ Server/GiantScape.Common/Game/GameEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GiantScape.Common.Game
+{
+    public abstract class GameEntity
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+
+        public GameEntity(string name = "")
+        {
+            Name = name;
+            ID = Guid.NewGuid().ToString();
+        }
+    }
+}
