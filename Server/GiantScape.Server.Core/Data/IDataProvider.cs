ffa8f27fc1aa1bddbd37fa32673cdbedbb638676
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-13 00:23:03 -0500

    Added basic IDataProvider interface

---
+++ Server/GiantScape.Server.Core/Data/IDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using GiantScape.Common.Game;
+using GiantScape.Common.Game.Tilemaps;
+
+namespace GiantScape.Server.Data
+{
+    internal interface IDataProvider
+    {
+        TilemapData GetMap();
+
+        void GetPlayerAsync(string username, Action<Player> callback);
+    }
+}
