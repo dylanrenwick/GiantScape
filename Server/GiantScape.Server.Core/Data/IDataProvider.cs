bfaf520efefdfbabc54b402322923ac4d8a4d661
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-13 03:14:50 -0500

    Changed mind on database approach, scrapped existing DAL work

--- Server/GiantScape.Server.Core/Data/IDataProvider.cs
+++ Server/GiantScape.Server.Core/Data/IDataProvider.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 
-using GiantScape.Common.Game;
 using GiantScape.Common.Game.Tilemaps;
 
 namespace GiantScape.Server.Data
 {
     internal interface IDataProvider
     {
-        TilemapData GetMap();
 
-        void GetPlayerAsync(string username, Action<PlayerEntity> callback);
     }
 }
