e0152f7302273f586e471393a67d86a5f1dd22fc
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-13 04:08:24 -0500

    Added DataStore

---
+++ Server/GiantScape.Server.Core/DataStores/DataStore.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json.Linq;
+
+namespace GiantScape.Server.DataStores
+{
+    internal partial class DataStore
+    {
+        public void SaveToFile(string filepath)
+        {
+            using (var fs = new FileStream(filepath, FileMode.OpenOrCreate))
+            using (var writer = new BsonDataWriter(fs))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(writer, this);
+            }
+        }
+
+        public static DataStore FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<DataStore>(json);
+        }
+        public static DataStore FromJson(JToken json)
+        {
+            return json.ToObject<DataStore>();
+        }
+    }
+}
---
+++ Server/GiantScape.Server.Core/DataStores/PlayerData.cs
@@ -0,0 +1,9 @@
+﻿using GiantScape.Common.Game;
+
+namespace GiantScape.Server.DataStores
+{
+    internal partial class DataStore
+    {
+        public Vector2Int Position { get; set; }
+    }
+}
