1a87a977ec395b395ccfcd3b5524343fd1bae03e
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-13 03:56:44 -0500

    Updated User Password property to PasswordHash

--- Server/GiantScape.Server.Core/Data/Models/User.cs
+++ Server/GiantScape.Server.Core/Data/Models/User.cs
@@ -5,7 +5,7 @@
         public string ID { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
 
         public UserLevel Level { get; set; }
     }
