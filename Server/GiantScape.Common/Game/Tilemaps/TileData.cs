3a05a909da496b5893232b66771cb08ecb53c871
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-17 01:01:02 -0500

    Added TileData

---
+++ Server/GiantScape.Common/Game/Tilemaps/TileData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GiantScape.Common.Game.Tilemaps
+{
+    [Serializable]
+    public class TileData
+    {
+        public string ResourceName { get; set; }
+
+        public TileCollision Collision { get; set; }
+
+        public float PathingWeight { get; set; }
+    }
+
+    public enum TileCollision : byte
+    {
+        /*     */ None = 0,
+        /*     */
+        /* |X  */ Left,
+        /*     */
+        /*  X| */ Right,
+        /*  _  */
+        /*  X  */ Up,
+        /*     */
+        /*  _  */ Down,
+        /*  _  */
+        /* |X  */ UpLeft,
+        /*  _  */
+        /*  X| */ UpRight,
+        /*     */
+        /*  _| */ DownRight,
+        /*     */
+        /* |_  */ DownLeft,
+        /*     */
+        /* |_| */ OpenUp,
+        /*  _  */
+        /* |_  */ OpenRight,
+        /*  _  */
+        /* |X| */ OpenDown,
+        /*  _  */
+        /*  _| */ OpenLeft,
+        /*  _  */
+        /* |_| */ Full
+    }
+}
