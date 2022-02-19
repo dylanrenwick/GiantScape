6a8f608c56f72cf398a01a2a613fcba2b4b542f6
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-18 23:59:44 -0500

    Removed map loading data from World, added support for multiple maps

--- Server/GiantScape.Common/Game/World.cs
+++ Server/GiantScape.Common/Game/World.cs
@@ -1,6 +1,5 @@
-﻿using System.IO;
-
-using Newtonsoft.Json;
+﻿using System;
+using System.Collections.Generic;
 
 using GiantScape.Common.Game.Tilemaps;
 using GiantScape.Common.Logging;
@@ -9,20 +8,30 @@ namespace GiantScape.Common.Game
 {
     public class World : Loggable
     {
-        private const string filepathFormat = "Resources/Tilemaps/{0}.json";
-        private const string filename = @"overworld";
+        private Dictionary<string, TilesetData> tilesets;
+        private Dictionary<string, Map> maps;
 
-        private TilemapData tilemapData;
-
-        public World(Logger log, string mapFilename = filename)
+        public World(Logger log)
             : base(log)
         {
-            string filepath = string.Format(filepathFormat, mapFilename);
-            log.Info($"Reading world data from '{filepath}'...");
-            string json = File.ReadAllText(filepath);
-            log.Debug("Parsing world json...");
-            tilemapData = JsonConvert.DeserializeObject<TilemapData>(json);
-            log.Debug($"Loaded {tilemapData.Layers.Length} map layers");
+            tilesets = new Dictionary<string, TilesetData>();
+            maps = new Dictionary<string, Map>();
+        }
+
+        public void RegisterTileset(TilesetData tileset)
+        {
+            if (!tilesets.ContainsKey(tileset.TilesetName)) tilesets.Add(tileset.TilesetName, tileset);
+            else throw new ArgumentException($"Tileset '{tileset.TilesetName}' already registered!");
+        }
+        public void LoadMap(TilemapData tilemap)
+        {
+            if (tilesets.ContainsKey(tilemap.Name)) throw new ArgumentException($"Tilemap with name '{tilemap.Name}' is already loaded!");
+            string tilesetName = tilemap.TilesetID;
+            if (!tilesets.ContainsKey(tilesetName)) throw new Exception($"Could not load tileset '{tilesetName}'");
+            TilesetData tileset = tilesets[tilesetName];
+
+            Map map = new Map(tilemap, tileset);
+            maps.Add(map.Name, map);
         }
 
         public TilemapData GetMapDataForPlayer(PlayerEntity player)
