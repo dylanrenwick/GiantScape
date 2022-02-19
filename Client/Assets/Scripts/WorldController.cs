cd321905170097bd8c6aef1bc7b35d8e8e08a73e
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-19 01:46:19 -0500

    Added WorldController component to handle game World

---
+++ Client/Assets/Scripts/WorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+using GiantScape.Common.Game;
+
+namespace GiantScape.Client
+{
+    public class WorldController : MonoBehaviour
+    {
+        private World world;
+
+        private void Start()
+        {
+            world = new World(UnityLogger.Instance.SubLogger("WORLD");
+        }
+    }
+}
