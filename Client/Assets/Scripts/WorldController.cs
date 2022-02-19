13d1c35c97baf3670025b4b5c382bf817e4f8fc1
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-19 01:53:32 -0500

    Added RegisterTileset proxy to WorldController

--- Client/Assets/Scripts/WorldController.cs
+++ Client/Assets/Scripts/WorldController.cs
@@ -7,12 +7,15 @@ using System.Threading.Tasks;
 using UnityEngine;
 
 using GiantScape.Common.Game;
+using GiantScape.Common.Game.Tilemaps;
 
 namespace GiantScape.Client
 {
     public class WorldController : MonoBehaviour
     {
         private World world;
+
+        public void RegisterTileset(TilesetData tileset) => world.RegisterTileset(tileset.TilesetName, tileset);
 
         private void Start()
         {
