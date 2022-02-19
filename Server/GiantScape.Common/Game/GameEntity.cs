56909c40bcd747b646b5c6e551b999b006c7214b
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-19 00:24:23 -0500

    Added MapID property to GameEntity

--- Server/GiantScape.Common/Game/GameEntity.cs
+++ Server/GiantScape.Common/Game/GameEntity.cs
@@ -6,11 +6,13 @@ namespace GiantScape.Common.Game
     {
         public string ID { get; private set; }
         public string Name { get; private set; }
+        public string MapID { get; private set; }
 
-        public GameEntity(string name = "")
+        public GameEntity(string mapID, string name = "")
         {
             Name = name;
             ID = Guid.NewGuid().ToString();
+            MapID = mapID;
         }
     }
 }
--- Server/GiantScape.Common/Game/PlayerEntity.cs
+++ Server/GiantScape.Common/Game/PlayerEntity.cs
@@ -2,6 +2,10 @@
 {
     public class PlayerEntity : GameEntity
     {
+        public PlayerEntity(string mapID, string name = "")
+            : base(mapID, name)
+        { }
+
         public Vector2Int Position { get; set; }
     }
 }
