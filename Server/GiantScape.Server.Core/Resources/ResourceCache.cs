1a39fc7aa126aa41f7b95c213872f90dc98fc7d1
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-18 23:36:27 -0500

    Added ResourceCache to handle server-side resource loading

---
+++ Server/GiantScape.Server.Core/Resources/ResourceCache.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GiantScape.Common;
+using GiantScape.Common.Game.Tilemaps;
+using GiantScape.Server.Data.Models;
+
+namespace GiantScape.Server.Resources
+{
+    internal class ResourceCache
+    {
+        private const string RES_ROOT = "Resources/";
+        private const string RES_TILEMAP = "Tilemaps/";
+        private const string RES_TILESET = "Tilesets/";
+
+        public static TilemapData LoadMap(Map map)
+        {
+            return LoadObject<TilemapData>($"{RES_ROOT}{RES_TILEMAP}{map.Filename}");
+        }
+
+        public static TilesetData LoadTileset(Tileset tileset)
+        {
+            return LoadObject<TilesetData>($"{RES_ROOT}{RES_TILESET}{tileset.Filename}");
+        }
+
+        private static T LoadObject<T>(string filepath)
+        {
+            if (TryLoadJson(filepath, out string json))
+            {
+                return Serializer.Deserialize<T>(json);
+            }
+            else if (TryLoadJsonc(filepath, out string jsonc))
+            {
+                return Serializer.Deserialize<T>(jsonc);
+            }
+            else throw new FileLoadException($"Could not load file at '{filepath}.json' or '{filepath}.jsonc'");
+        }
+
+        private static bool TryLoadJson(string filepath, out string json) => TryLoadFile(filepath + ".json", out json);
+        private static bool TryLoadJsonc(string filepath, out string json) => TryLoadFile(filepath + ".jsonc", out json);
+
+        private static bool TryLoadFile(string filepath, out string contents)
+        {
+            if (File.Exists(filepath))
+            {
+                contents = File.ReadAllText(filepath);
+                return true;
+            }
+
+            contents = string.Empty;
+            return false;
+        }
+    }
+}
