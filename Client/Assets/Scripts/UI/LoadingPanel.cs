c43acaf22ea3d0b293b14ce72074747562b8ea1f
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-19 01:40:03 -0500

    Added LoadingPanel component for managing loading panel

---
+++ Client/Assets/Scripts/UI/LoadingPanel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GiantScape.Client.UI
+{
+    public class LoadingPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private Text loadingText;
+        [SerializeField]
+        private Slider loadingBar;
+
+        public void Show() => SetVisibility(true);
+        public void Hide() => SetVisibility(false);
+        public void SetVisibility(bool vis)
+        {
+            loadingText.gameObject.SetActive(vis);
+            loadingBar.gameObject.SetActive(vis);
+        }
+
+        public void SetState(string text, float barValue)
+        {
+            loadingText.text = text;
+            loadingBar.value = barValue;
+        }
+    }
+}
