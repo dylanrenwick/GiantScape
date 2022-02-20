fecc5b0266b907ec4463b3f886fdb0f7c7c69499
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-19 22:32:46 -0500

    Added GameClient class to act as client-side parallel to GameServer

---
+++ Client/Assets/Scripts/GameClient.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+
+using GiantScape.Client.Net;
+using GiantScape.Common.Game;
+using GiantScape.Common.Logging;
+using GiantScape.Common.Net;
+
+namespace GiantScape.Client
+{
+    internal class GameClient : Loggable
+    {
+        private NetworkClient networkClient;
+
+        private World world;
+
+        private string address;
+        private ushort port;
+
+        public GameClient(string address, ushort port, Logger log)
+            : base(log)
+        {
+            this.address = address;
+            this.port = port;
+
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            var conn = new NetworkConnection(socket, log);
+            networkClient = new NetworkClient(conn, log);
+
+            world = new World(log.SubLogger("GAMECL"));
+        }
+
+        public void Start()
+        {
+            networkClient.BeginConnect(address, port);
+        }
+    }
+}
