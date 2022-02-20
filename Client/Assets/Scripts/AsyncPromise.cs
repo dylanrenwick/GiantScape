c90d1165bb04b127d76ac321474bcfed4cd3545d
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-19 23:08:13 -0500

    Created AsyncPromise for promise-pattern async behaviour

---
+++ Client/Assets/Scripts/AsyncPromise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GiantScape.Client
+{
+    internal class AsyncPromise
+    {
+        public event EventHandler Done;
+
+        private bool done;
+        public bool IsDone
+        {
+            get => done;
+            set
+            {
+                done = value;
+                if (done) OnDone();
+            }
+        }
+
+        protected virtual void OnDone()
+        {
+            Done?.Invoke(this, null);
+        }
+    }
+
+    internal class AsyncPromise<T> : AsyncPromise
+    {
+        public new event EventHandler<T> Done;
+
+        public T Result { get; set; }
+
+        protected override void OnDone()
+        {
+            Done?.Invoke(this, Result);
+        }
+    }
+}
