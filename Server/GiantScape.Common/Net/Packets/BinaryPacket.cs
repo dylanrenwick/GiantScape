c426e6c6b1bb358380f6b80f5775229c87ba1329
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-26 01:35:13 -0500

    Made content public in BinaryPacket

--- Server/GiantScape.Common/Net/Packets/BinaryPacket.cs
+++ Server/GiantScape.Common/Net/Packets/BinaryPacket.cs
@@ -5,33 +5,33 @@ namespace GiantScape.Common.Net.Packets
     public class BinaryPacket : NetworkPacket
     {
         public override PacketType Type => type;
-
-        public override ushort PacketLength => (ushort)content.Length;
-
-        private byte[] content;
         private readonly PacketType type = PacketType.None;
 
-        public override byte[] GetContentBytes() => content;
+        public override ushort PacketLength => (ushort)Content.Length;
 
-        public override void FromBytes(byte[] bytes) => content = bytes;
+        public byte[] Content { get; private set; }
+
+        public override byte[] GetContentBytes() => Content;
+
+        public override void FromBytes(byte[] bytes) => Content = bytes;
 
         public BinaryPacket()
         {
-            this.content = Array.Empty<byte>();
+            this.Content = Array.Empty<byte>();
         }
         public BinaryPacket(PacketType type)
         {
             this.type = type;
-            this.content = Array.Empty<byte>();
+            this.Content = Array.Empty<byte>();
         }
         public BinaryPacket(byte[] content)
         {
-            this.content = content;
+            this.Content = content;
         }
         public BinaryPacket(PacketType type, byte[] content)
         {
             this.type = type;
-            this.content = content;
+            this.Content = content;
         }
     }
 }
