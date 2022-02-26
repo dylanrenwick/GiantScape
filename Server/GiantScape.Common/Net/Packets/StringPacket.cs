6402eb4927c6ac5ec8ee047b35df5588cfdbe172
Dylan Renwick <skidsirongenesisdev@gmail.com>
2022-02-26 01:28:33 -0500

    Added StringPacket

---
+++ Server/GiantScape.Common/Net/Packets/StringPacket.cs
@@ -0,0 +1,32 @@
+﻿namespace GiantScape.Common.Net.Packets
+{
+    public class StringPacket : NetworkPacket
+    {
+        private PacketType type;
+        public override PacketType Type => type;
+
+        public override ushort PacketLength => PacketEncoding.GetContentsSize(StringVal);
+
+        public string StringVal { get; private set; }
+
+        public override void FromBytes(byte[] bytes)
+        {
+            StringVal = PacketEncoding.BytesToString(bytes);
+        }
+
+        public override byte[] GetContentBytes()
+        {
+            return PacketEncoding.StringToBytes(StringVal);
+        }
+
+        public StringPacket(PacketType type)
+        {
+            this.type = type;
+        }
+        public StringPacket(PacketType type, string str)
+        {
+            this.type = type;
+            StringVal = str;
+        }
+    }
+}
